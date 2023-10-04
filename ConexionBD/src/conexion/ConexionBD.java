2381ab8475822e4272d2f9d4e543dee995b4c3b4
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2023-10-04 11:42:37 +0200

    FIX estaba mal el .jar de mysql

--- ConexionBD/src/conexion/ConexionBD.java
+++ ConexionBD/src/conexion/ConexionBD.java
@@ -2,19 +2,22 @@ package conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.TimeZone;
 
 public class ConexionBD {
     private Connection conexion;
-    public void ConexionBD() throws SQLException {
+    public ConexionBD() throws SQLException {
         String host = "localhost";
         String baseDatos = "aeropuertos";
         String usuario = "admin";
-        String password = "mipassword";
+        String password = "password";
         String cadenaConexion = "jdbc:mysql://" + host + "/" + baseDatos+ "?serverTimezone=" + TimeZone.getDefault().getID();
         conexion = DriverManager.getConnection(cadenaConexion, usuario, password);
         conexion.setAutoCommit(true);
+
     }
     public Connection getConexion() {
         return conexion;
